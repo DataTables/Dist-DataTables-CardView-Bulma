d1c7c01cd926339dd5b92a281610ddb9f3ac54b0
Allan Jardine <allan@datatables.net>
2026-06-23 15:21:51 +0000

    Initial commit

---
+++ js/cardView.bulma.min.mjs
@@ -0,0 +1,4 @@
+/*! CardView Bulma styling 0.0.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
+ */
+import DataTable from"datatables.net-bm";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"card dtcv-card",cardContent:"card-content"});export default DataTable;
