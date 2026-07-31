ab56a327ded9f86eefb261ad7127d7e99b37468d
dtbuild <dtbuild@rigil>
2026-07-31 08:37:01 +0000

    220865f41de48460b4a9c7613b2956518344e915 Release 1.0.1
    Sync to source repo @220865f41de48460b4a9c7613b2956518344e915

--- js/cardView.bulma.min.mjs
+++ js/cardView.bulma.min.mjs
@@ -1,4 +1,4 @@
-/*! CardView Bulma styling 1.0.0 for DataTables
+/*! CardView Bulma styling 1.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-bm";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"card dtcv-card",cardContent:"card-content",container:"dtcv-container content"});export default DataTable;
